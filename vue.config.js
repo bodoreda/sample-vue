bbb00dffbcb727aa7510befce7738972f37fbd29
BH_desktop <bodoreda@gmail.com>
2023-03-15 23:45:28 +0900

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: "../../back-end/sample/src/main/resources/static",  // D:\workspace\back-end\sample\src\main\resources\static
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true, //  cross-origin 허용
+      }
+    }
+  }
 })
